c595547ff918a322851dfdf94d22c72dcdafdf8d
samunpovt <sotavov@gmail.com>
2015-09-27 22:26:58 +0300

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.ReadKey();
-            Console.ReadKey();
+            char c=Console.ReadKey();
             Console.ReadKey();
         }
     }
