fa985d824fb255c00a804c5ae478bc4c169a72f6
samun <sotavov@unecon.ru>
2015-09-27 22:27:17 +0300

    Работа 5

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.ReadKey();
-            Console.ReadKey();
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
